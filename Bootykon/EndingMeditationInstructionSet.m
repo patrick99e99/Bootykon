75ffdec7e0ae6094cf940b01b3d213ec5be6626c
Patrick Collins <patrick@collinatorstudios.com>
2017-09-17 17:58:17 -0700

    add bow in end of class

--- Bootykon/EndingMeditationInstructionSet.m
+++ Bootykon/EndingMeditationInstructionSet.m
@@ -9,12 +9,15 @@
              [Instruction delay:10],
              [Instruction setDialogPath:@"Audio/Dialog/Meditation"],
              [Instruction dialog:@"cooldown"],
+             [Instruction delay:5],
              [Instruction choices:@[
                  [Instruction dialog:@"outro-1"],
                  [Instruction dialog:@"outro-2"],
                  [Instruction dialog:@"outro-3"],
              ]],
-             [Instruction delay:60],
+             [Instruction delay:15],
+             [Instruction dialog:@"bow"],
+             [Instruction delay:10],
              [Instruction fadeOutMusic],
     ];
 }
